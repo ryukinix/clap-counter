e3f1920c691d355cfe152887df493e85120ea708
Manoel Vilela <manoel_vilela@engineer.com>
2019-03-10 10:12:26 -0300

    Refactor monadic Counter implementation
    CHANGELOG:
    + Implements getS to unwrap the value of the state monad
    + Write a exemple of recursive "mutation" of the counter monad
    + Write the zeroState and zeroCounter primitives
    
    TODO:
    + Write a main using do notation with logging after each
      mutation.

--- src/Counter.hs
+++ src/Counter.hs
@@ -2,16 +2,38 @@ module Counter where
 
 import Control.Monad.State
 
-newtype Counter = MkCounter {cValue :: Int}
+newtype Counter = MkCounter {cValue :: Integer}
   deriving (Show)
 
 -- | 'inc c n' increments the counter by 'n' units.
-inc :: Counter -> Int -> Counter
+zeroCounter :: Counter
+zeroCounter = MkCounter 0
+
+inc :: Counter -> Integer -> Counter
 inc (MkCounter c) n = MkCounter (c + n)
 
--- | CounterS is a monad.
-type CounterS = State Counter
+-- | Unwrap the integer of Counter
+cnt :: Counter -> Integer
+cnt (MkCounter c) = c
 
 -- | Increment the counter by 'n' units.
-incS :: Int-> CounterS ()
+incS :: Integer -> State Counter ()
 incS n = modify (\c -> inc c n)
+
+-- | Get the inner state of State Counter monad instance
+getS :: State Counter () -> Integer
+getS = cnt . snd . (flip runState) zeroCounter
+
+-- | zeroState
+zeroState :: State Counter ()
+zeroState = incS 0
+
+-- | Example of mutation using the monadic counter
+mutation :: Integer -> State Counter ()
+mutation n
+  | n <= 0 = zeroState
+  | otherwise  = incS 1 >> (mutation (n - 1))
+
+
+main :: IO ()
+main = print $ getS $ mutation 30
